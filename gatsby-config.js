de6769c280d9f0d4754757603f1e9d88d7897ffa
Aditya Prabhat <adityaprabhat907@gmail.com>
2022-03-31 22:54:43 +0530

    added portfolio data

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,12 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Aditya Prabhat`,
+    siteTitleAlt: `Aditya Prabhat - portfolio`,
+    siteHeadline: `Aditya Prabhat`,
+    siteDescription: `I am Aditya Prabhat`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteImage: ``,
   },
   plugins: [
     {
@@ -22,6 +20,10 @@ module.exports = {
       // See the theme's README for all available options
       options: {
         navigation: [
+          {
+            title: `Projects`,
+            slug: `/projects`,
+          },
           {
             title: `Blog`,
             slug: `/blog`,
@@ -34,11 +36,15 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/AdityaPrabhat09`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Github`,
+            url: `https://github.com/adityaprabhat01`,
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/aditya-prabhat-4aa395184/`,
           },
         ],
       },
